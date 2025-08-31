92deca7688f51829af032e0a16d129590aa689c1
Salwa <fijrisalwa@gmail.com>
2025-08-31 16:11:28 +0700

    FILE PROJECT1

---
+++ Salwa_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.salwa_project1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Salwa_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Salwa Fijri Rahayu");
+        System.out.println("NPM: 2420506019");
+        System.out.println("Alamat: Magelang Utara, Kota Magelang");
+        System.out.println("No HP: 0123456789");
+    }
+}
